9a1ed43bf159252ec328faad2423f94a616a1fb9
unknown <15372028397@163.com>
2026-06-03 15:37:59 +0800

    20260603 first step To learn from graet blog

--- src/content/ledger.ts
+++ src/content/ledger.ts
@@ -11,12 +11,11 @@ export const ledgerEntries: LedgerEntry[] = [
     publishedAt: '2026-06-02T16:30:00+08:00',
     title: '碎碎念',
     body: `感觉自己不再年轻了哦。`
+  },
+  {
+    id: 'second-ledger-row',
+    publishedAt: '2026-06-02T16:30:00+08:00',
+    title: '钱钱',
+    body: `本科学历不知道学费多少。`
   }
 ];
-
-{
-  id: '2nd-ledger-row',
-  publishedAt: '2026-06-02T16:30:00+08:00',
-  title: '钱钱',
-  body: `本科学历不知道学费多少`
-}
